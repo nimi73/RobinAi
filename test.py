48d854bcd3c7b99ca3f6d83f7886c45b897eeb1d
nimi73 <99484051+nimi73@users.noreply.github.com>
2023-07-20 18:34:36 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import
+random
+key0 = []
+while len(key0) < 10:
+    while True:
+        candidate = random.randint(0, 9)
+        if candidate in key0:
+            continue
+        key0.append(candidate)
+
+key1 = []
+while len(key1) < 10:
+    while True:
+        candidate = random.randint(10, 19)
+        if candidate in key1:
+            continue
+        key1.append(candidate)
